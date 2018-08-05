f1a07355a1c0e11b25c821dc5f1457a1a9478f81
cplayer-2018 <39280173+cplayer-2018@users.noreply.github.com>
2018-08-05 11:30:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# 2018-8-1 03:10:18
+# 2018-8-5 11:30:15
 import time,easygui as eg
 
 s = 1800
@@ -9,3 +9,9 @@ for i in range(1800):
 	print('事件',Event,'，剩余',s,'秒。')
 	time.sleep(1)
 eg.msgbox(('事件',Event,'，30 分钟倒计时结束。'), '倒计时提醒', '好的')
+
+""" Lite
+for i in range(1799):
+	time.sleep(1)
+eg.msgbox('倒计时结束。')
+"""
